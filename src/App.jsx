767693e76727cc988f952ff5a218e96492694dae
cannon0074 <lamrof_roku@yahoo.co.jp>
2024-09-04 00:41:48 +0900

    ああああああ

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Header from "./Components/header.jsx";
 import Main from "./Components/main.jsx";
 import "./App.css";
